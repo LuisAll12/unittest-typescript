2d8bff1d8b9e117fc77831f1964efd0fa97f6ba9
Luis Allamand <Luis@g-12.ch>
2025-03-21 15:17:17 +0100

    Aufgabe 4.1

--- src/calculator.test.ts
+++ src/calculator.test.ts
@@ -1,4 +1,4 @@
-import { add, subtract, multiply } from "./calculator";
+import { add, subtract, multiply, divide} from "./calculator";
 
 test("add three to two is five", () => {
   // Arrange
@@ -34,6 +34,19 @@ test("multiply five by seven is thirty-five", () => {
 
   // Act
   const actual: number = multiply(a, b);
+
+  // Assert
+  expect(actual).toBe(expected);
+});
+
+test("divide ten by two is five", () => {
+  // Arrange
+  const a: number = 10;
+  const b: number = 2;
+  const expected: number = 5;
+
+  // Act
+  const actual: number = divide(a, b);
 
   // Assert
   expect(actual).toBe(expected);
